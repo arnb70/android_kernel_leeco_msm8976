fc43b7287bf5b1743960a6235dea00c92376a10f
PutraGandaD <putraganda.dewata@gmail.com>
2020-07-20 00:49:49 +0700

    msm: kgsl: Report correct GPU frequency in sysfs
    by @myfluxi

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -792,7 +792,6 @@ static ssize_t kgsl_pwrctrl_gpuclk_show(struct device *dev,
 	if (device == NULL)
 		return 0;
 	pwr = &device->pwrctrl;
-
 	if (device->state == KGSL_STATE_SLUMBER)
 		freq = pwr->pwrlevels[pwr->num_pwrlevels - 1].gpu_freq;
 	else
